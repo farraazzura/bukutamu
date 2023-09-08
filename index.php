c11b916e6d5f585d5e6ec2c4710906f07273bd02
farraazzura <faraazzura611@gmail.com>
2023-09-08 10:40:30 +0700

    latihan pwpb 5.1

---
+++ index.php
@@ -0,0 +1,178 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Buku Tamu</title>
+    <style>
+        html{
+            sroll-behavior: smooth;
+        }
+    </style>
+  </head>
+  <body>
+    <!-- Bagian Navbar -->
+    <section id="nav">
+        <nav class="navbar navbar-expand-lg navbar-light ">
+        <div class="container">
+             <a class="navbar-brand" href="#"><span>BukuTamu</span></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+             <span class="navbar-toggler-icon"></span>
+          </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+             <li class="nav-item active">
+               <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+             <li class="nav-item">
+               <a class="nav-link" href="#">Program Keahlian</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link" href="#">Visi Misi</a>
+             </li>
+             <li class="nav-item">
+                <a class="nav-link" href="#">Hubungi Kami</a>
+             </li>
+           </ul>
+         </div>
+       </nav>
+     </section>
+     <!-- bagian banner -->
+     <section id="banner">
+        <div class="banner">
+            <div class="container">
+                <div class="row">
+                  <div class="col-md-6">
+                    <h4 class="title">Selamat Datang di SMK Negri 9 Malang</h4>
+                    <p>Terima Kasih Telah Berkunjung ke Buku Tamu ini. Semoga anda sehat selalu</p>
+                  </div>
+                  <div class="col-md-6">
+                        <img src="assets/images/head.png" class="ban-img img-fluid">
+                     </div>
+                </div>
+            </div>
+        </div>
+        <svg class="wave"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,288L80,288C160,288,320,288,480,256C640,224,800,160,960,149.3C1120,139,1280,181,1360,202.7L1440,224L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+</section>
+<!-- Bagian Program Keahlian -->
+<section id="progkal">
+    <div class="container">
+    <h2 class="text-center"> PROGRAM KEAHLIAN </h2>
+    <div class="row text-center">
+        <div class="col-md-3 progkal">
+            <img src="assets/images/tbsm.png" class="img-fluid">
+            <h4>Teknik Bisnis Sepeda Motor</h4>
+            <p>Menghasilkan tenaga bersertifikasi dan kompeten untuk mengisi lowongan kerja di Teknik Otomotif Sepeda Motor, Jasa Perawatan Sepeda Motor dan mampu bersaing di Tingkat Nasional Maupun Internasional.</p>
+          </div>
+          <div class="col-md-3 progkal">
+            <img src="assets/images/tkj.png" class="img-fluid">
+            <h4>Teknik Komputer Jaringan</h4>
+            <p>Menghasilkan tenaga bersertifikasi dan kompetensi untuk mengisi lowongan kerja di Bidang Teknik Informatika dan Komputer dan mampu bersaing di Tingkat Nasional Maupun Internasional.</p>
+      </div>
+      <div class="col-md-3 progkal">
+            <img src="assets/images/rpl.png" class="img-fluid">
+            <h4>Rekayasa Perangkat Lunak</h4>
+            <p>Menghasilkan tenaga bersertifikasi dan kompetensi di bidang Rekayasa Perangkat Lunak untuk mengisi lowongan kerja di Bidang Teknik Informatika dan Komputer dan mampu bersaing di Tingkat Regional maupun Nasional.</p>
+      </div>
+      <div class="col-md-3 progkal">
+            <img src="assets/images/anm.png" class="img-fluid">
+            <h4>Animasi</h4>
+            <p>Menghasilkan tenaga kerja yang berkompeten untuk mengisi lowongan kerja di bidang Animasi dan mampu bersaing di tingkat Regional maupun Nasional.
+</p>
+      </div>
+       </div>
+   </div>
+</section>
+<!-- Bagian Visi Misi -->
+<section id="visimisi">
+    <div class="container">
+        <h2 class="text-center">Visi dan Misi</h2>
+        <div class="row">
+        <div class="col-md-6">
+     <h4>Visi Sekolah</h4>
+     <p>MEWUJUDKAN LEMBAGA PENDIDIKAN DAN PELATIHAN KEJURUAN, YANG MENGHASILKAN LULUSAN YANG BERAKHLAK MULIA, KOMPETEN DAN BERWAWASAN LIGKUNGAN , SERTA MAMPU BERSAING DI TINGKAT NASIONAL MAUPUN INTERNASIONAL.</p>
+     <br>
+     <h4>Misi Sekolah</h4>
+     <ol>
+        <li>Mewujudkan Tenaga Pendidik, Kependidikan , dan lulusan yang berbudi pekerti dan berakhlak mulia</li>
+        <li>Mengelola manajemen sekolah dengan standar ISO 9001 : 2015.</li>
+        <li>Melaksanakan sistem pendidikan dan mengupayakan mutu layanan pendidikan sesuai tuntutan masyarakat dengan memanfaatkan teknologi informasi dan komunikasi</li>
+        <li>Mewujudkan lingkungan sekolah menuju sekolah adiwiyata serta mengembangkan nilai-nilai kewirausahaan warga sekolah.</li>
+     </ol>
+     <div class="col-md-6">
+            <img src="assets/images/target.png" class="img-fluid">
+             </div>
+         </div>
+     </div>
+</div>
+</section>
+
+<!-- Bagian Contact -->
+<section id="contact">
+    <div class="container">
+        <h2 clas="text-center">HUBUNGI KAMI </h2>
+        <div class="row contact">
+            <div class="col-md-4">
+                <img src="assets/images/contact-us.png" class="img-fluid">
+            </div>
+            <div class="col-md-8">
+                <form action="proses.php" method="post">
+                    <div class="form-group">
+                      <label>No KTP</label>
+                      <input type="number" class="form-control" name="noktp" placeholder="No KTP Anda...">
+                    </div>
+                    <div class="form-group">
+                      <label>No handphone</label>
+                      <input type="number" class="form-control" name="nohp" placeholder="No HP Anda...">
+                    </div>
+                    <div class="form-group">
+                      <label>Nama</label>
+                      <input type="text" class="form-control" name="nm" placeholder="Nama Anda...">
+                    </div>
+                    <div class="form-group">
+                      <label>Alamat</label>
+                      <textarea class="form-control" rows="3" name="alm" placeholder="Alamat Anda..."></textarea>
+                    </div>
+                    <button class="btn btn-primary w-100" type="submit">KIRIM</button>
+</form>
+            </div>
+        </div>
+    </div>
+
+</section>
+<!-- Bagian Footer -->
+<section id="footer">
+    <div class="container"> 
+        <p class="text-center">Copyright &copy;2021</p>
+    </div>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href"#">Home<span classs+"sr-only>"()
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#progkal">Program Keahlian></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#visimisi">Visi Misi></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#contact">Hubungi Kami></a>
+            </li>
+        </ul>
+
+    </div>
+</section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
